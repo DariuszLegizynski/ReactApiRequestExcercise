2cad914b94657a1096deb33c296dd83f771af7c8
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-02-17 11:28:22 +0100

    API request without axios.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState();
+	const address = "https://randomuser.me/api";
+
+	useEffect(() => {
+		const getData = async () => {
+			const response = await fetch(address).catch((err) =>
+				alert(err.message)
+			);
+			const fetchedData = await response
+				.json()
+				.catch((err) => alert(err.message));
+			setData(fetchedData.results);
+		};
+		getData();
+	}, []);
+	let name = "";
+	if (data) {
+		name = data.map((el) => el.name.first);
+	}
+
+	return <div className="App">Hello {name}</div>;
 }
 
 export default App;

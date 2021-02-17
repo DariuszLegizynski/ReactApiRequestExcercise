c87364470a4702af43fee68ea8d9e4b001890097
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-02-17 13:47:30 +0100

    making API request with use of axios.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+import axios from "axios";
+
 function App() {
 	const [data, setData] = useState();
 	const address = "https://randomuser.me/api";
 
+	// useEffect(() => {
+	// 	const getData = async () => {
+	// 		const response = await fetch(address).catch((err) =>
+	// 			alert(err.message)
+	// 		);
+	// 		const fetchedData = await response
+	// 			.json()
+	// 			.catch((err) => alert(err.message));
+	// 		setData(fetchedData.results);
+	// 	};
+	// 	getData();
+	// }, []);
+
 	useEffect(() => {
-		const getData = async () => {
-			const response = await fetch(address).catch((err) =>
-				alert(err.message)
-			);
-			const fetchedData = await response
-				.json()
+		const fetchedData = async () => {
+			axios
+				.get(address)
+				.then((response) => {
+					setData(response.data.results);
+				})
 				.catch((err) => alert(err.message));
-			setData(fetchedData.results);
 		};
-		getData();
+		fetchedData();
 	}, []);
+
+	console.log(data);
+
 	let name = "";
 	if (data) {
 		name = data.map((el) => el.name.first);
